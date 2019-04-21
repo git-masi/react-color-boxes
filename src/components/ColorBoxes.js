e9a467df970ba343192a48a64cae6659c38f638f
Eric Masi <eric@gitmasi.com>
2019-04-21 10:27:42 -0400

    change color to HSL for easier future manipulation

--- src/components/ColorBoxes.js
+++ src/components/ColorBoxes.js
@@ -3,10 +3,11 @@ import './ColorBoxes.css';
 import ColorBox from './ColorBox';
   
 class ColorBoxes extends Component {
+
   render() {
     return (
       <div>
-        <ColorBox color="pink"/>
+        <ColorBox color="hsl(158, 55%, 78%)"/>
       </div>
     )
   }
