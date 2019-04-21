6f4a677a92290f67d6100281a439c00fc28b33a0
Eric Masi <eric@gitmasi.com>
2019-04-21 12:02:23 -0400

    test multiple colorbox components for random color

--- src/components/ColorBoxes.js
+++ src/components/ColorBoxes.js
@@ -3,11 +3,25 @@ import './ColorBoxes.css';
 import ColorBox from './ColorBox';
   
 class ColorBoxes extends Component {
+  constructor() {
+    super();
+    this.state = {
+      baseColors: ['hsl(158, 55%, 78%)', 'hsl(83, 55%, 84%)', '	hsl(24, 100%, 86%)', 'hsl(3, 100%, 82%)', 'hsl(355, 100%, 77%)']
+    }
+    this.randomColor = this.randomColor.bind(this);
+  }
+
+  randomColor = function() {
+    return this.state.baseColors[(Math.floor(Math.random() * this.state.baseColors.length))];
+  }
 
   render() {
     return (
       <div>
-        <ColorBox color="hsl(158, 55%, 78%)"/>
+        <ColorBox color={this.randomColor()}/>
+        <ColorBox color={this.randomColor()}/>
+        <ColorBox color={this.randomColor()}/>
+        <ColorBox color={this.randomColor()}/>
       </div>
     )
   }
